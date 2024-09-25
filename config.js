22826492bc34ec0d339b30b37a0a48c7f355de15
shinjizen <rhadityahdw3@gmail.com>
2024-09-25 09:35:07 +0700

    aps hubs browser

---
+++ config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+let { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_CALLBACK_URL, SERVER_SESSION_SECRET, PORT } = process.env
+if (!APS_CLIENT_ID || !APS_CLIENT_SECRET || !APS_CALLBACK_URL || !SERVER_SESSION_SECRET) {
+    console.warn('Missing some of the environment variables.');
+    process.exit(1);
+}
+PORT = PORT || 8080;
+
+module.exports = {
+    APS_CLIENT_ID,
+    APS_CLIENT_SECRET,
+    APS_CALLBACK_URL,
+    SERVER_SESSION_SECRET,
+    PORT
+};
